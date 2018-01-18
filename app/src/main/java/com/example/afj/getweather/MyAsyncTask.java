2dcc2abc022a2f51d7e8b48a9252691720d19018
a-fujimiya <a_fujimiya@waku-2.com>
2018-01-18 18:40:55 +0900

    wip create project

---
+++ app/src/main/java/com/example/afj/getweather/MyAsyncTask.java
@@ -0,0 +1,17 @@
+package com.example.afj.getweather;
+
+import android.os.AsyncTask;
+
+/**
+ *
+ */
+
+public class MyAsyncTask extends AsyncTask<Void, Void, String> {
+  public MyAsyncTask() {
+    super();
+  }
+
+  @Override protected String doInBackground(final Void... params) {
+    return null;
+  }
+}
